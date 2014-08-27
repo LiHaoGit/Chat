4737294b2a9e50f15ee9934e3c5b2d205ed87ed7
lihao <dahao@qq.com>
2014-08-27 21:21:09 +0800

    聊天功能简单实现

---
+++ MyChat/Hub/Chat.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyChat
+{
+    public class Chat:Hub
+    {
+        public void SendToAll(string name,string message)
+        {
+            Clients.All.SendToAllMessage(name, message);
+        }
+
+        public void SendToGroup(string name,string message,string groupName)
+        {
+            Clients.Group(groupName).SendToGroupMessage(name, message);
+        }
+    }
+}
