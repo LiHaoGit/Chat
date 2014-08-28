8d1fd137d31aeae17115f333b029a6d85f6ae57a
lihao <dahao@qq.com>
2014-08-28 22:49:53 +0800

    重写界面

--- MyChat/Hub/Chat.cs
+++ MyChat/Hub/Chat.cs
@@ -3,19 +3,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace MyChat
 {
     public class Chat:Hub
     {
+        JavaScriptSerializer js = new JavaScriptSerializer();
         public void SendToAll(string name,string message)
         {
-            Clients.All.SendToAllMessage(name, message);
+            
+            
+            Clients.All.SendToAllMessage(js.Serialize(new { Name = name, Message = message}));
         }
 
         public void SendToGroup(string name,string message,string groupName)
         {
             Clients.Group(groupName).SendToGroupMessage(name, message);
         }
+
+        /// <summary>
+        /// 当有一个新连接的时候执行
+        /// </summary>
+        /// <returns></returns>
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            //Clients.Others.SendToAllMessage(Context.ConnectionId,"连接了"+"---"+Context.ConnectionId);
+           
+            return base.OnConnected();
+        }
+        
     }
 }
